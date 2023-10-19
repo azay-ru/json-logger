0b3513d500b5bfe7ea9ecfe437217d806d871987
Zaynutdinov Aleksey <zayaa@el-plat.ru>
2023-10-19 15:40:00 +0400

    Add log levels

--- README.md
+++ README.md
@@ -17,6 +17,20 @@ Installation is possible using Composer.
 composer require azay/json-logger
 ```
 
+## Log levels
+Logger supports the logging levels described by [RFC 5424](https://datatracker.ietf.org/doc/html/rfc5424).
+
+| Level  |Severity|Description|Name in Logger| Constant in Logger |
+|:------:|-|-|-|-------------|
+|   0    |Emergency|system is unusable|`emergency`| EMERGENCY |
+|   1    |Alert|action must be taken immediately|`alert`| ALERT       |
+|   2    |Critical|critical conditions|`critical`| CRITICAL    |
+|   3    |Error|error conditions|`error`| ERROR       |
+|   4    |Warning|warning conditions|`warning`| WARNING     |
+|   5    |Notice|normal but significant condition|`notice`| NOTICE      |
+|   6    |Informational|informational messages|`info`| INFO        |
+|   7    |Debug|debug-level messages|`debug`| DEBUG       |
+
 ## Usage
 ```php
 use Azay\Log\JsonLogger;
@@ -27,10 +41,28 @@ $logger = new JsonLogger(
     JsonLogger::DEFAULT_TIME_FORMAT
 );
 
+//Optional, set maximum severity level, default `debug`
+$logger->setLevel('info');
+// or as constant
+$logger->setLevel($logger::INFO);
+
 $logger->info(
     'Info message for json logger',
     [
-        'Foo' => 'Boo'
+        'Foo' => 'Bar'
     ]
 );
+
+// This message will be ignored by severity
+$logger->debug(
+    'Verbose debug message',
+    [
+        'details' => 'Lorem ipsum...'
+    ]
+);
+```
+
+### Output
+```json
+{"time":"2023-10-19 11:29:34","level":"info","message":"Info message for json logger","Foo":"Bar"}
 ```
--- src/JsonLogger.php
+++ src/JsonLogger.php
@@ -3,60 +3,128 @@
 namespace Azay\Log;
 
 use Psr\Log\AbstractLogger;
+use Psr\Log\InvalidArgumentException;
+use Psr\Log\LogLevel;
 
 /**
  * One of the simplest implementations PSR-3 compatible Json logger
  */
 class JsonLogger extends AbstractLogger
 {
+    const EMERGENCY = 0;
+    const ALERT = 1;
+    const CRITICAL = 2;
+    const ERROR = 3;
+    const WARNING = 4;
+    const NOTICE = 5;
+    const INFO = 6;
+    const DEBUG = 7;
+    const LEVELS = [
+            self::EMERGENCY => LogLevel::EMERGENCY,
+            self::ALERT     => LogLevel::ALERT,
+            self::CRITICAL  => LogLevel::CRITICAL,
+            self::ERROR     => LogLevel::ERROR,
+            self::WARNING   => LogLevel::WARNING,
+            self::NOTICE    => LogLevel::NOTICE,
+            self::INFO      => LogLevel::INFO,
+            self::DEBUG     => LogLevel::DEBUG
+    ];
+
     const DEFAULT_OPTIONS = JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE;
     const DEFAULT_TIME_FORMAT = 'Y-m-d H:i:s';
     const BREAKS = "\n";
     const DUPLICATE_PREFIX = 'context_';
 
+    /**
+     * @var string $filename
+     */
     private $filename;
+    /**
+     * @var int $options
+     */
     private $options;
+    /**
+     * @var string $timeFormat
+     */
     private $timeFormat;
+    /**
+     * @var string $string
+     */
     private $break;
+    /**
+     * @var int $level
+     */
+    private $level = 7;
 
     /**
      * @param string $filename
-     * @param int $options JSON decoding options
-     * @param string $timeFormat Time format ,
-     * @param bool $insertBreaks Insert breaks line after each record
+     * @param int    $options      JSON decoding options
+     * @param string $timeFormat   Time format ,
+     * @param bool   $insertBreaks Insert breaks line after each record
      */
     public function __construct(string $filename, int $options = self::DEFAULT_OPTIONS, string $timeFormat = self::DEFAULT_TIME_FORMAT, bool $insertBreaks = true)
     {
-        $this->filename = $filename . (empty(pathinfo($filename, PATHINFO_EXTENSION)) ? '.json' : '');
+        $this->filename = $filename . (empty(pathinfo($filename, PATHINFO_EXTENSION)) ? '.json':'');
         $this->options = $options;
         $this->timeFormat = $timeFormat;
         $this->break = $insertBreaks
-            ? self::BREAKS
-            : '';
+                ? self::BREAKS
+                :'';
     }
 
-    public function log($level, $message='', array $context = [])
+    public function log($level, $message = '', array $context = [])
     {
+        $currentLevel = $this->getLevel($level);
+
+        if ($currentLevel > $this->level) {
+            return;
+        }
+
         $record = [
-            'time' => date($this->timeFormat),
-            'level' => $level,
+                'time'  => date($this->timeFormat),
+                'level' => self::LEVELS[$currentLevel]
         ];
 
-        if (!empty($message))
+        if ( ! empty($message)) {
             $record['message'] = $message;
+        }
 
         foreach ($context as $key => $value) {
             $recordKey = array_key_exists($key, $record)
-                ? self::DUPLICATE_PREFIX . $key
-                : $key;
+                    ? self::DUPLICATE_PREFIX . $key
+                    :$key;
             $record[$recordKey] = $value;
         }
 
         file_put_contents(
-            $this->filename,
-            json_encode($record, $this->options) . $this->break,
-            FILE_APPEND
+                $this->filename,
+                json_encode($record, $this->options) . $this->break,
+                FILE_APPEND
         );
-
     }
+
+    public function setLevel($level)
+    {
+        $this->level = $this->getLevel($level);
+    }
+
+    private function getLevel($level): int
+    {
+        if (is_int($level)) {
+            return max(self::EMERGENCY, min(self::DEBUG, $level));
+        }
+
+        if (is_string($level)) {
+            $lower = strtolower($level);
+            $index = array_search($lower, self::LEVELS);
+
+            if ($index === false)
+                throw new InvalidArgumentException('Level "' . $level . '" is not defined');
+
+            return $index;
+        }
+
+        throw new InvalidArgumentException('Level "' . var_export($level, true) . '" is not integer or string');
+    }
+
 }
